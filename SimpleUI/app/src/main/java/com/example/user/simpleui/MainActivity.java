64b89d6761ed8e5e5a550ba6772f4682fd5a83c5
shaun316229 <103316229@gms.tcu.edu.tw>
2016-08-09 11:29:06 +0800

    add teabutton

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textview;
+    EditText editText;
+    RadioGroup radioGroup;
+    String drink="Black Tea";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +20,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textview=(TextView)findViewById(R.id.textview);
+        editText=(EditText)findViewById(R.id.editText);
+        radioGroup=(RadioGroup)findViewById(R.id.radiogroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.blacktearadiobutton) {
+                    drink = "Black Tea";
+                } else if (checkedId == R.id.greentearadioButton2)
+                {
+                    drink = "Green Tea";
+                }
+            }
+        });
 
     }
 
     public void click(View view)
     {
-        textview.setText("Hello TextView!!!");
+        String text=editText.getText().toString();
+        text=text+"Order"+drink;
+        textview.setText(text);
     }
 }
