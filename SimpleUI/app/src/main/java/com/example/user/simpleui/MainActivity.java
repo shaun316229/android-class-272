327beae13e21caf78062eed107ab89c6800a41ed
shaun316229 <103316229@gms.tcu.edu.tw>
2016-08-10 09:56:27 +0800

    add listview

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textview;
     EditText editText;
     RadioGroup radioGroup;
+    ListView listView;
     String drink="Black Tea";
 
     @Override
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         textview=(TextView)findViewById(R.id.textview);
         editText=(EditText)findViewById(R.id.editText);
         radioGroup=(RadioGroup)findViewById(R.id.radiogroup);
+        listView=(ListView)findViewById(R.id.listView);
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -34,7 +38,16 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        setupListView();
 
+    }
+
+    private void setupListView()
+    {
+        String[] data=new String[]{"1","2","3","4","5","6","7","8"};
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,data);
+        listView.setAdapter(adapter);
     }
 
     public void click(View view)
